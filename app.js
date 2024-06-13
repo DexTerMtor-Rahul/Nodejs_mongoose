c518c8ac3c0da607a3a79ae375c28efd3f371bb1
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-06-13 23:09:12 +0530

    wrapup with mongoose CRUD functions

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const mongoose = require('mongoose');
 const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,9 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('6669a73ca6070775b772f478')
+  User.findById('666b1e2503f7ba2b54f3e416')
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
@@ -34,6 +34,26 @@ app.use(shopRoutes);
 
 app.use(errorController.pageNotfound);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://dextermtor:dextermtor@cluster0.0m4h3tp.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0',
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Rahul',
+          email: 'rahul@gmail.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
